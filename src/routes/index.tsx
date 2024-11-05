e929c81dbe2ea8374eac7bd5d00ac2ff0623ba2d
Chris Cooper <cjcooper252@gmail.com>
2024-11-04 16:17:05 -0800

    clean up types

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,44 +10,34 @@ export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
-type GameStage =
-  | "waitingForUser"
-  | "houseIsChoosing"
-  | "waitingForResult"
-  | "showResult";
+const DELAY_MS = 1000;
+
 type Result = "win" | "lose" | "draw" | undefined;
 
 function determineResult(userPick: Weapon, housePick: Weapon): Result {
   if (!userPick || !housePick) return;
-  if (userPick.name === housePick.name) return "draw";
-  if (userPick.name === "Paper" && housePick.name === "Rock") return "win";
-  if (userPick.name === "Rock" && housePick.name === "Scissors") return "win";
-  if (userPick.name === "Scissors" && housePick.name === "Paper") return "win";
-  return "lose";
+  if (userPick.beats === housePick.name) return "win";
+  if (housePick.beats === userPick.name) return "lose";
+  return "draw";
 }
 
-// Do we need game stage?
 function HomeComponent() {
   const [score, setScore] = useState(0);
   const [userPick, setUserPick] = useState<Weapon | undefined>();
   const [housePick, setHousePick] = useState<Weapon | undefined>();
-  const [gameStage, setGameStage] = useState<GameStage>("waitingForUser");
   const [result, setResult] = useState<Result>();
 
   const handleUserPick = (userPick: Weapon) => {
     setUserPick(userPick);
-    setGameStage("houseIsChoosing");
 
     setTimeout(() => {
       const randomIndex = Math.floor(Math.random() * WEAPONS.length);
       const housePick = WEAPONS[randomIndex];
       setHousePick(housePick);
-      setGameStage("waitingForResult");
 
       setTimeout(() => {
         const result = determineResult(userPick, housePick);
         setResult(result);
-        setGameStage("showResult");
         setScore((prevScore: number) => {
           if (result === "win") {
             return prevScore + 1;
@@ -57,12 +47,11 @@ function HomeComponent() {
             return prevScore;
           }
         });
-      }, 1000);
-    }, 1000);
+      }, DELAY_MS);
+    }, DELAY_MS);
   };
 
   const resetGame = () => {
-    setGameStage("waitingForUser");
     setHousePick(undefined);
     setUserPick(undefined);
     setResult(undefined);
@@ -71,7 +60,7 @@ function HomeComponent() {
   return (
     <div className="flex min-h-screen flex-col justify-center gap-24">
       <Scoreboard score={score} />
-      {gameStage === "waitingForUser" ? (
+      {!userPick ? (
         <Choices onUserPick={handleUserPick} />
       ) : (
         <Game
--- src/weapons-data.tsx
+++ src/weapons-data.tsx
@@ -3,16 +3,19 @@ export const WEAPONS = [
     name: "Paper",
     primaryColour: "indigo-500",
     icon: "images/icon-paper.svg",
+    beats: "Rock",
   },
   {
     name: "Scissors",
     primaryColour: "amber-500",
     icon: "images/icon-scissors.svg",
+    beats: "Paper",
   },
   {
     name: "Rock",
     primaryColour: "rose-600",
     icon: "images/icon-rock.svg",
+    beats: "Scissors",
   },
 ] as const;
 
