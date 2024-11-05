feede2143487dd857dbff60166ba7e3aba30320a
Chris Cooper <cjcooper252@gmail.com>
2024-11-05 09:34:10 -0800

    rename to public

--- src/weapons-data.tsx
+++ src/weapons-data.tsx
@@ -2,19 +2,19 @@ export const WEAPONS = [
   {
     name: "Paper",
     primaryColour: "indigo-500",
-    icon: "assets/images/icon-paper.svg",
+    icon: "/images/icon-paper.svg",
     beats: "Rock",
   },
   {
     name: "Scissors",
     primaryColour: "amber-500",
-    icon: "assets/images/icon-scissors.svg",
+    icon: "/images/icon-scissors.svg",
     beats: "Paper",
   },
   {
     name: "Rock",
     primaryColour: "rose-600",
-    icon: "assets/images/icon-rock.svg",
+    icon: "/images/icon-rock.svg",
     beats: "Scissors",
   },
 ] as const;
